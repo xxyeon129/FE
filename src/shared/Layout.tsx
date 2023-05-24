a6b2b100f4a6ada3a3a0f1c641034a8f2909e995
xxyeon129 <xxyeon129@gmail.com>
2023-05-24 17:40:55 +0900

    style: import 대소문자 변경

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -1,4 +1,4 @@
-import TestNav from '@src/components/main/testNav';
+import TestNav from '@src/components/main/TestNav';
 import { styled } from 'styled-components';
 import { ReactNode } from 'react';
 
