edf53e7914b4c029d74252444b5411563b8afa19
Undong00 <ehddjs85246@gmail.com>
2023-05-31 01:21:02 +0900

    style : AutoSearch에 대한 주석처리

--- src/components/AutoSearch.tsx
+++ src/components/AutoSearch.tsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import axios from 'axios';
 // 딜레이를 줄이기 위한 것 =  디바이스  라이브러리 : lodash
 function AutoSearch() {
+  // 검색어 저장
   const [searchTerm, setSearchTerm] = useState('');
+  // 서버에서 받은 검색어 저장
   const [suggestions, setSuggestions] = useState([]);
 
+  //input의 요소 값이 변경될 때마다 호출
   const handleChange = async e => {
     const term = e.target.value;
     setSearchTerm(term);
@@ -19,6 +22,7 @@ function AutoSearch() {
     }
   };
 
+  // 제안된 검색어를 클릭했을 때 함수
   const handleClickSuggestion = suggestion => {
     setSearchTerm(suggestion);
   };
