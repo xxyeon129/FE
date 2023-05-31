c5411c03a26b69dc9caa429b33242c49220eb2ed
Undong00 <ehddjs85246@gmail.com>
2023-05-31 16:50:47 +0900

    style : 검색 결과 페이지 board line css 적용

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { portfolioDataState } from '@src/states/SearchResultsState';
-import AutoSearch from '@src/components/AutoSearch';
 import { styled } from 'styled-components';
 
 const SearchResults = () => {
   const portfolioData = useRecoilValue(portfolioDataState);
   return (
     <div>
-      <AutoSearch />
       <h1>Portfolio Page</h1>
       {portfolioData.map((portfolio, index) => (
-        <div key={index}>
+        <Stboard key={index}>
           <div>{portfolio.portfolioImage}</div>
           <h3>{portfolio.portfolioTitle}</h3>
           <p>{portfolio.userName}</p>
           <p>{portfolio.userProfileImage}</p>
-        </div>
+        </Stboard>
       ))}
     </div>
   );
