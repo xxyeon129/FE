28db71fcb001f5b2ce052bdc4a8d528e7dba2b49
Undong00 <ehddjs85246@gmail.com>
2023-05-31 15:17:49 +0900

    add : 검색 결과 페이지에 포트폴리어 데이터 렌더링 부분 추가

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { portfolioDataState } from '@src/states/SearchResultsState';
+import AutoSearch from '@src/components/AutoSearch';
+import { styled } from 'styled-components';
 
-function SearchResults() {
-  return <div></div>;
-}
+const SearchResults = () => {
+  const portfolioData = useRecoilValue(portfolioDataState);
+  return (
+    <div>
+      <AutoSearch />
+      <h1>Portfolio Page</h1>
+      {portfolioData.map((portfolio, index) => (
+        <div key={index}>
+          <div>{portfolio.portfolioImage}</div>
+          <h3>{portfolio.portfolioTitle}</h3>
+          <p>{portfolio.userName}</p>
+          <p>{portfolio.userProfileImage}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default SearchResults;
+
+const Stboard = styled.div`
+  border: 1px solid black;
+`;
