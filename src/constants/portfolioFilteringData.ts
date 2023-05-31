6f26274508d1e81ded361b3b6326cccff0226251
xxyeon129 <xxyeon129@gmail.com>
2023-05-31 10:00:18 +0900

    refactor: 직군 카테고리, 직무 필터 전체 키워드  상수화

--- src/constants/portfolioFilteringData.ts
+++ src/constants/portfolioFilteringData.ts
@@ -1,8 +1,76 @@
-export const categoryList = ['All', 'Develop', 'Design', 'Photographer'];
+export const CATEGORY_KEYWORD = {
+  ALL: 'All',
+  DEVELOP: 'Develop',
+  DESIGN: 'Design',
+  PHOTOGRAPHER: 'Photographer',
+};
+
+export const FILTER_KEYWORD = {
+  ALL: 'All',
+  DEVELOP: {
+    BACKEND: 'Backend',
+    FRONTEND: 'Frontend',
+    AI: 'AI',
+    BIG_DATA: 'Big Data',
+    APP: 'App',
+    SYSTEM: 'System',
+    SECURITY: 'Security',
+  },
+  DESIGN: {
+    GRAPHIC: 'Graphic',
+    UIUX: 'UI/UX',
+    WEB: 'Web',
+    VISUAL: 'Visual',
+    INTERACTION: 'Interaction',
+    PRODUCT: 'Product',
+    BRAND: 'Brand',
+  },
+  PHOTOGRAPH: {
+    COMMERCIAL: 'Commercial',
+    PORTRAIT: 'Portrait',
+    WEDDING: 'Wedding',
+    FASHION: 'Fashion',
+    WILDLIFE: 'Wildlife',
+    SPORTS: 'Sports',
+  },
+};
+
+export const categoryList = [
+  CATEGORY_KEYWORD.ALL,
+  CATEGORY_KEYWORD.DEVELOP,
+  CATEGORY_KEYWORD.DESIGN,
+  CATEGORY_KEYWORD.PHOTOGRAPHER,
+];
 
 export const filterListObject = {
   all: [],
-  develop: ['All', 'Backend', 'Frontend', 'AI', 'Big Data', 'App', 'System', 'Security'],
-  design: ['All', 'Graphic', 'UI/UX', 'Web', 'Visual', 'Interaction', 'Product', 'Brand'],
-  photograph: ['All', 'Commercial', 'Portrait', 'Wedding', 'Fashion', 'Wildlife', 'Sports'],
+  develop: [
+    FILTER_KEYWORD.ALL,
+    FILTER_KEYWORD.DEVELOP.BACKEND,
+    FILTER_KEYWORD.DEVELOP.FRONTEND,
+    FILTER_KEYWORD.DEVELOP.AI,
+    FILTER_KEYWORD.DEVELOP.BIG_DATA,
+    FILTER_KEYWORD.DEVELOP.APP,
+    FILTER_KEYWORD.DEVELOP.SYSTEM,
+    FILTER_KEYWORD.DEVELOP.SECURITY,
+  ],
+  design: [
+    FILTER_KEYWORD.ALL,
+    FILTER_KEYWORD.DESIGN.GRAPHIC,
+    FILTER_KEYWORD.DESIGN.UIUX,
+    FILTER_KEYWORD.DESIGN.WEB,
+    FILTER_KEYWORD.DESIGN.VISUAL,
+    FILTER_KEYWORD.DESIGN.INTERACTION,
+    FILTER_KEYWORD.DESIGN.PRODUCT,
+    FILTER_KEYWORD.DESIGN.BRAND,
+  ],
+  photograph: [
+    FILTER_KEYWORD.ALL,
+    FILTER_KEYWORD.PHOTOGRAPH.COMMERCIAL,
+    FILTER_KEYWORD.PHOTOGRAPH.PORTRAIT,
+    FILTER_KEYWORD.PHOTOGRAPH.WEDDING,
+    FILTER_KEYWORD.PHOTOGRAPH.FASHION,
+    FILTER_KEYWORD.PHOTOGRAPH.WILDLIFE,
+    FILTER_KEYWORD.PHOTOGRAPH.SPORTS,
+  ],
 };
