b248bf4a222031e9d89d59327b04346957d4ca12
xxyeon129 <xxyeon129@gmail.com>
2023-06-01 06:28:58 +0900

    style: 임시 test token 코드 분리

--- src/apis/portfolio.ts
+++ src/apis/portfolio.ts
@@ -1,7 +1,8 @@
 import apiRequest from '.';
 import { GetLastIdParams, GetAllListParams, GetFilteredListParams } from '@src/types/apiParamsType';
+// TEST CODE
+import { testLoginToken, testRefreshToken } from './testToken';
 
-// TODO: 서버 배포 시 API 형식에 맞게 수정
 const RESOURCE = '/api/portfolios';
 
 export const getLastId = async ({ category, filter }: GetLastIdParams) => {
@@ -72,12 +73,6 @@ export const getFilteredList = async ({
 
 export const createPortfolio = async (formData: FormData) => {
   try {
-    // TEST CODE
-    const testLoginToken =
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZW1haWxAZ21haWwuY29tIiwidXNlcklkIjo0LCJleHAiOjE2ODU0NDU0NTUsImlhdCI6MTY4NTQ0MTg1NX0.YwNR7to5tms2KAGW0wKgdU-omsv95aT1kbcvnXf4cJE';
-    const testRefreshToken =
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZW1haWxAZ21haWwuY29tIiwidXNlcklkIjo0LCJleHAiOjE2ODY2NTE0NTUsImlhdCI6MTY4NTQ0MTg1NX0.QRKZf8KKb-9sAALkyYwl3ynEAM8a-sdN3kaxR_-B0Xo';
-
     const response = await apiRequest.post(RESOURCE, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
--- src/apis/project.ts
+++ src/apis/project.ts
@@ -1,11 +1,11 @@
 import apiRequest from '.';
+// TEST CODE
+import { testLoginToken } from './testToken';
 
 const RESOURCE = '/api/projects';
 
 export const createProject = async (formData: FormData) => {
   try {
-    const testLoginToken =
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZW1haWxAZ21haWwuY29tIiwidXNlcklkIjo0LCJleHAiOjE2ODU1NzA5MTAsImlhdCI6MTY4NTU2NzMxMH0.TbTbmBgT9slKinGyIzoYLPleslNGbmosWBgd6sg6lXM';
     const response = await apiRequest.post(RESOURCE, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
