d084349aeaaf32f2b9aaee9be90304f810fd9575
Undong00 <ehddjs85246@gmail.com>
2023-05-31 15:08:38 +0900

    add : 라우터 SearchResults 연결 추가

--- src/shared/MainRouter.tsx
+++ src/shared/MainRouter.tsx
@@ -6,6 +6,7 @@ import MyPage from '@src/pages/MyPage';
 import { PATH_URL } from '@src/constants/constants';
 import Layout from './Layout';
 import TestPortfolioPage from '@src/pages/TestPortfolioPage';
+import SearchResults from '@src/pages/SearchResults';
 
 const MainRouter = () => {
   return (
@@ -17,6 +18,7 @@ const MainRouter = () => {
           <Route path={PATH_URL.TEST} element={<Test />} />
           <Route path="/test/portfolio" element={<TestPortfolioPage />} />
           <Route path={PATH_URL.MYPAGE} element={<MyPage />} />
+          <Route path={PATH_URL.SEARCHRESULTS} element={<SearchResults />} />
         </Routes>
       </Layout>
     </BrowserRouter>
