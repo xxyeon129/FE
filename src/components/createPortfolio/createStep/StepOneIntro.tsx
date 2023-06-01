27cdfb742744c68e699eb15a5c01fffbf9b77bbc
xxyeon129 <xxyeon129@gmail.com>
2023-06-01 16:31:23 +0900

    style: StepOneIntro styled-components 변수명 가독성 개선, UI 조정, 주석처리 코드 삭제

--- src/components/createPortfolio/createStep/StepOneIntro.tsx
+++ src/components/createPortfolio/createStep/StepOneIntro.tsx
@@ -26,47 +26,24 @@ const StepOneIntro = ({ onNextButtonClick }: StepOneProps) => {
 
   return (
     <StStepOneContainer>
-      <StDescriptionContainer>
+      <StContentContainer>
         <StDescription>
           폴과 함께
           <br />
           포트폴리오를 입력해볼까요?
         </StDescription>
-        <StDescriptionDetailContainer>
+        <StDetailContainer>
           {descriptionDetail.map(detail => (
-            <StDescriptionDetailItem key={detail.id}>
+            <StDetailItem key={detail.id}>
               <StLabelContainer>
                 <StLabelNumber>{detail.id}</StLabelNumber>
-                <StDescriptionDetailLabel>{detail.label}</StDescriptionDetailLabel>
+                <StLabel>{detail.label}</StLabel>
               </StLabelContainer>
-              <StDescriptionDetailText>{detail.text}</StDescriptionDetailText>
-            </StDescriptionDetailItem>
+              <StDetailText>{detail.text}</StDetailText>
+            </StDetailItem>
           ))}
-          {/* <StDescriptionDetailItem>
-            <StDescriptionDetailLabel>
-              1 &nbsp;포트폴리오 기본 정보를 입력해주세요
-            </StDescriptionDetailLabel>
-            <StDescriptionDetailText>
-              표시될 포트폴리오 제목과 주요 사용 기술, 카테고리 등 기본 정보를 알려주세요.
-            </StDescriptionDetailText>
-          </StDescriptionDetailItem>
-          <StDescriptionDetailItem>
-            <StDescriptionDetailLabel>
-              2 &nbsp;포트폴리오 추가 정보를 입력해주세요
-            </StDescriptionDetailLabel>
-            <StDescriptionDetailText>
-              포트폴리오 대표 이미지를 제출하고 Youtube, Blog 링크를 추가해주시면 포트폴리오가 더
-              돋보일 수 있어요.
-            </StDescriptionDetailText>
-          </StDescriptionDetailItem>
-          <StDescriptionDetailItem>
-            <StDescriptionDetailLabel>3 &nbsp;등록을 완료하세요</StDescriptionDetailLabel>
-            <StDescriptionDetailText>
-              참여하신 프로젝트가 있다면 프로젝트를 입력하고 등록을 완료하세요.
-            </StDescriptionDetailText>
-          </StDescriptionDetailItem> */}
-        </StDescriptionDetailContainer>
-      </StDescriptionContainer>
+        </StDetailContainer>
+      </StContentContainer>
       <StButtonContainer>
         <StNextStepButton onClick={() => onNextButtonClick(STEP.TWO)}>시작하기</StNextStepButton>
       </StButtonContainer>
@@ -83,7 +60,7 @@ const StStepOneContainer = styled.div`
   padding: 0 41px;
 `;
 
-const StDescriptionContainer = styled.div`
+const StContentContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;
@@ -96,11 +73,11 @@ const StDescription = styled.h1`
   padding-bottom: 100px;
 `;
 
-const StDescriptionDetailContainer = styled.div`
+const StDetailContainer = styled.div`
   margin-right: 70px;
 `;
 
-const StDescriptionDetailItem = styled.div`
+const StDetailItem = styled.div`
   border-bottom: 1px solid lightgray;
 
   &:last-child {
@@ -110,15 +87,16 @@ const StDescriptionDetailItem = styled.div`
 
 const StLabelContainer = styled.div`
   display: flex;
+  margin-top: 5px;
 `;
 
-const StDescriptionDetailLabel = styled.h3``;
+const StLabel = styled.h3``;
 
 const StLabelNumber = styled.h3`
   margin-right: 9px;
 `;
 
-const StDescriptionDetailText = styled.div`
+const StDetailText = styled.div`
   color: gray;
   padding-left: 18px;
   max-width: 450px;
