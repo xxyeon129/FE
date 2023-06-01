4dfbc19959b8e338597adfadabf63fd4dbf8ee91
xxyeon129 <xxyeon129@gmail.com>
2023-06-02 03:33:46 +0900

    test: 포트폴리오 작성 마지막 단계에서 전역 관리 상태 테스트

--- src/components/createPortfolio/createStep/Step08Image.tsx
+++ src/components/createPortfolio/createStep/Step08Image.tsx
@@ -1,9 +1,25 @@
 import NextStepButton from '@src/components/common/createPortfolio/NextStepButton';
 import { StContainer } from '@src/components/common/createPortfolio/createStepStyles';
+import { createCategoryState, createFilterState, createTitleState } from '@src/states';
+import { useRecoilValue } from 'recoil';
 
 const Step08Image = () => {
+  const portfolioTitle = useRecoilValue(createTitleState);
+  const category = useRecoilValue(createCategoryState);
+  const filter = useRecoilValue(createFilterState);
+  // const residence = useRecoilValue()
+  // const location = useRecoilValue()
+  // const telephone = useRecoilValue()
+  // const email = useRecoilValue()
+  // const techStack = useRecoilValue()
+  // const projectIdList = useRecoilValue()
+  // const experience = useRecoilValue()
+  // const githubId = useRecoilValue()
+  // const youtubeUrl = useRecoilValue()
+  // const blogUrl = useRecoilValue()
+
   const onSubmitData = () => {
-    console.log('click');
+    console.log(`portfolioTitle: ${portfolioTitle}, category: ${category}, filter: ${filter}`);
   };
 
   return (
