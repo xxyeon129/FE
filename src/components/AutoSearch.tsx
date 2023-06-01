d2b6006b9d6258119f72e0651220340b0d48f033
Undong00 <ehddjs85246@gmail.com>
2023-06-01 15:22:01 +0900

    style: api 분리에 따른 코드 수정 및 searchTerm 리코일 추가

--- src/components/AutoSearch.tsx
+++ src/components/AutoSearch.tsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useRecoilState } from 'recoil';
-import { portfolioDataState } from '@src/states/SearchResultsState';
+import { portfolioDataState, searchTermState } from '@src/states/SearchResultsState';
 import { useNavigate } from 'react-router-dom';
 import { debounce } from 'lodash';
+import { getPage } from '@src/apis/pagenation';
 
 const AutoSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [suggestions, setSuggestions] = useState([]);
   const [portfolioData, setPortfolioData] = useRecoilState(portfolioDataState);
+  const [searchwords, setSearchWords] = useRecoilState(searchTermState);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -33,15 +35,10 @@ const AutoSearch = () => {
 
   const handleKeyDown = async e => {
     if (e.key === 'Enter') {
-      try {
-        const response = await axios.get(
-          `http://3.34.102.60:8080/api/portfolios/search?keyword=${searchTerm}`
-        );
-        setPortfolioData(response.data.data.content);
-        navigate('/searchresults');
-      } catch (error) {
-        console.error(error);
-      }
+      const portData = await getPage(1, searchTerm);
+      setPortfolioData(portData);
+      setSearchWords(searchTerm);
+      navigate('/searchresults');
     }
   };
 
