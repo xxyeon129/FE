489acd7e515b47ffa5bbaa2c41e096f48b09b0d5
운동 <112850163+Undong00@users.noreply.github.com>
2023-06-02 20:44:34 +0900

    fix : 병합 충돌 해결

--- src/shared/MainRouter.tsx
+++ src/shared/MainRouter.tsx
@@ -5,16 +5,13 @@ import Detail from '@src/pages/Detail';
 import MyPage from '@src/pages/MyPage';
 import { PATH_URL } from '@src/constants/constants';
 import Layout from './Layout';
-<<<<<<< HEAD
-import TestPortfolioPage from '@src/pages/TestPortfolioPage';
 import SearchResults from '@src/pages/SearchResults';
-=======
 import HyoHwan from '@src/pages/HyoHwan';
 import PortfolioDetails from '@src/pages/PortfolioDetails';
 import LinkPreview from '@src/components/LinkPreview';
 import MyPortfolio from '@src/pages/MyPortfolio';
 import CreatePortfolio from '@src/pages/CreatePortfolio';
->>>>>>> b49f464235e19fba45d6039ff0fe316dbc807626
+
 
 const MainRouter = () => {
   return (
@@ -27,14 +24,10 @@ const MainRouter = () => {
           <Route path={PATH_URL.HYO} element={<HyoHwan />} />
           <Route path={PATH_URL.PORTFOLIODETAIL} element={<PortfolioDetails />} />
           <Route path={PATH_URL.MYPAGE} element={<MyPage />} />
-<<<<<<< HEAD
           <Route path={PATH_URL.SEARCHRESULTS} element={<SearchResults />} />
-=======
           <Route path={PATH_URL.HYO2} element={<LinkPreview />} />
-          <Route path={PATH_URL.MYPAGE} element={<MyPage />} />
           <Route path={PATH_URL.MYPORTFOLIO} element={<MyPortfolio />} />
           <Route path={PATH_URL.CREATE_PORTFOLIO} element={<CreatePortfolio />} />
->>>>>>> b49f464235e19fba45d6039ff0fe316dbc807626
         </Routes>
       </Layout>
     </BrowserRouter>
