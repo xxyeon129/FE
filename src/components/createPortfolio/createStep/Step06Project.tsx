64e5b157a6da13097c4c092efed19215c0049444
xxyeon129 <xxyeon129@gmail.com>
2023-06-07 08:33:07 +0900

    feat/refactor: 포트폴리오 작성 - step 6 프로젝트 삭제, 유저 이미지 추가, 컴포넌트 분리

--- src/components/createPortfolio/createStep/Step06Project.tsx
+++ src/components/createPortfolio/createStep/Step06Project.tsx
@@ -6,16 +6,19 @@ import { createProjectIdListState, createProjectListState } from '@src/states';
 import { CreatePortfolioStepProps } from '@src/types/portfolioType';
 import { useEffect, useState } from 'react';
 import { useRecoilState, useSetRecoilState } from 'recoil';
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 import TestCreateProjectModal from '../TestCreateProjectModal';
 import { useQueryClient } from 'react-query';
 import { ProjectDataType } from '@src/types/portfolioType';
 import PrevStepButton from '@src/components/common/createPortfolio/PrevStepButton';
+import ProjectItem from '@src/components/project/ProjectItem';
+import { deleteProject } from '@src/apis/project';
 
 const Step06Project = ({ onNextButtonClick, onPrevButtonClick }: CreatePortfolioStepProps) => {
-  const setProjectIdList = useSetRecoilState<number[]>(createProjectIdListState);
+  const [projectIdList, setProjectIdList] = useRecoilState<number[]>(createProjectIdListState);
   const [projectList, setProjectList] = useRecoilState<ProjectDataType[]>(createProjectListState);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isProjectExist, setIsProjectExist] = useState<boolean>(false);
 
   const queryClient = useQueryClient();
   const projectData = queryClient.getQueryData('projectData') as ProjectDataType | null;
@@ -28,31 +31,53 @@ const Step06Project = ({ onNextButtonClick, onPrevButtonClick }: CreatePortfolio
     e.stopPropagation();
   };
 
+  const onClickdeleteProject = (id: number) => {
+    deleteProject(id);
+    const deletedList = projectList.filter(project => project.id !== id);
+    setProjectList(deletedList);
+    const deletedIdList = projectIdList.filter(projectId => projectId !== id);
+    setProjectIdList(deletedIdList);
+  };
+
   useEffect(() => {
     if (projectData) {
+      setIsProjectExist(true);
       setProjectList(prevProject => [...prevProject, projectData]);
       setProjectIdList(prevProjectIds => [...prevProjectIds, projectData.id]);
     }
   }, [projectData]);
 
+  useEffect(() => {
+    projectList.length !== 0 && setIsProjectExist(true);
+    projectList.length === 0 && setIsProjectExist(false);
+  }, [projectList]);
+
   const title = '프로젝트를 추가하세요';
   const description =
-    '진행한 프로젝트를 추가하고 설명을 작성해보세요.\n작성하신 프로젝트는 포트폴리오 작성 후 언제든 수정하실 수 있습니다.';
+    '진행하신 프로젝트를 추가하고 설명을 작성해보세요.\n추가된 프로젝트의 오른쪽 상단 X 아이콘을 눌러 삭제하실 수 있습니다.\n작성하신 프로젝트는 포트폴리오 작성 후 언제든 수정하실 수 있으니 편하게 작성해주세요!';
 
   return (
     <S.Container>
       <TitleTextLabel title={title} description={description} containerWidth="900px" />
       <StAddProjectContainer>
         <StAddProjectButton onClick={handleModal}>+ 프로젝트 추가</StAddProjectButton>
-        <StProjectsList>
+        <StProjectsList isprojectexist={`${isProjectExist}`}>
           {projectList?.length !== 0 &&
             projectList.map(project => (
-              <StProjectItem key={project.id}>
-                <StProjectImg src={project.projectImageList[0].imageUrl} />
-                <StProjectText>{project.title}</StProjectText>
-                {/* TODO: 로그인 후 userId 확인 방법 생기면 회원조회로 profileImg, nickname 가져와서 추가 */}
-              </StProjectItem>
+              <ProjectItem
+                key={project.id}
+                project={project}
+                deleteProject={onClickdeleteProject}
+                isEditMode={true}
+              />
             ))}
+          {!isProjectExist && (
+            <StNoProjectText>
+              작성하신 프로젝트가 없습니다.
+              <br />
+              상단 프로젝트 추가 버튼을 눌러 프로젝트를 추가해주세요!
+            </StNoProjectText>
+          )}
         </StProjectsList>
       </StAddProjectContainer>
 
@@ -79,36 +104,31 @@ const StAddProjectButton = styled.button`
   color: ${({ theme }) => theme.color.lightGreen};
 `;
 
-const StProjectsList = styled.div`
+const StProjectsList = styled.div<{ isprojectexist: string }>`
   width: 900px;
   min-height: 250px;
 
   padding: 1rem;
-  border: 1px solid gray;
-  border-radius: 10px;
 
   display: flex;
-  gap: 1rem;
+  gap: 20px;
   flex-flow: wrap;
+
+  ${({ isprojectexist }) =>
+    isprojectexist === 'false' &&
+    css`
+      border: 1px dashed gray;
+      border-radius: 20px;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      line-height: 160%;
+      font-weight: bold;
+    `}
 `;
 
-const StProjectItem = styled.div`
-  background-color: lightgrey;
-  border-radius: 10px;
-  padding: 1rem;
-  width: 200px;
-  height: 250px;
-`;
-
-const StProjectImg = styled.img`
-  width: 100%;
-  height: 80%;
-  object-fit: cover;
-`;
-
-const StProjectText = styled.div`
-  font-weight: bold;
-  margin-top: 0.5rem;
+const StNoProjectText = styled.div`
+  color: #9d9d9d;
 `;
 
 const StBackground = styled.div`
