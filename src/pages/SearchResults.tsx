e64d5413790ba06448a3bb93c8475470909225a1
Undong00 <ehddjs85246@gmail.com>
2023-06-07 04:45:57 +0900

    style : 검색 결과 페이지 UI 추가

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -5,11 +5,14 @@ import { styled } from 'styled-components';
 import { searchPage } from '@src/apis/search';
 import { searchTermState } from '@src/states/SearchResultsState';
 import { useNavigate } from 'react-router-dom';
+import { PortfolioDataType } from '@src/types/portfolioType';
+import { ReactComponent as DefaultUserImage } from '@src/assets/nav/nav-default-user-image-icon.svg';
 // api 테스트 및 기능 구현 완료
 const SearchResults = () => {
   const [portfolioData, setPortfolioData] = useState();
   const searchTermData = useRecoilValue(searchTermState);
   const navigate = useNavigate();
+  const noImageUrl = 'public/images/no-img.jpg';
 
   const handlePageButtonClick = async index => {
     const pageData = await searchPage(index, searchTermData);
@@ -29,25 +32,44 @@ const SearchResults = () => {
   }, [searchTermData]);
 
   return (
-    <div>
-      <h1>Search Result</h1>
+    <>
+      <StHeader>
+        <h1>Search Result</h1>
+      </StHeader>
       {portfolioData && portfolioData.content.length > 0 ? (
         <>
-          {searchTermData ? (
-            <h2>'{searchTermData}' 기술 보유 포트폴리오 입니다.</h2>
-          ) : (
-            <h2>모든 포트폴리오입니다.</h2>
-          )}
-          {portfolioData.content.map((portfolio, index) => (
-            <Stboard key={index} onClick={() => onClickHandler(portfolio.id)}>
-              <h3>{portfolio.portfolioTitle}</h3>
-              <p>{portfolio.userName}</p>
-              <img src={portfolio.userProfileImage} alt="User Profile Image" />
-            </Stboard>
-          ))}
+          <StHeader>
+            {searchTermData ? (
+              <h2>'{searchTermData}' 기술 보유 포트폴리오 입니다.</h2>
+            ) : (
+              <h2>모든 포트폴리오입니다.</h2>
+            )}
+          </StHeader>
+          <StLayout>
+            {portfolioData.content.map((portfolio, index) => (
+              <StItemContainer key={index} onClick={() => onClickHandler(portfolio.id)}>
+                <StImgContainer>
+                  {portfolio.userProfileImage ? (
+                    <StPortfolioImg src={portfolio.userProfileImage} alt="User Profile Image" />
+                  ) : (
+                    <StNoImg src={noImageUrl} alt="No Image" />
+                  )}
+                </StImgContainer>
+                <StDescriptionContainer>
+                  <StUserDescriptionContainer>
+                    <StUserDefaultImage />
+                    <StUserNameText>{portfolio.userName}</StUserNameText>
+                  </StUserDescriptionContainer>
+                  <StTitleText>{portfolio.portfolioTitle}</StTitleText>
+                </StDescriptionContainer>
+              </StItemContainer>
+            ))}
+          </StLayout>
         </>
       ) : (
-        <h2>'{searchTermData}'에 대한 포트폴리오가 없습니다.</h2>
+        <StHeader>
+          <h2>'{searchTermData}'에 대한 포트폴리오가 없습니다.</h2>
+        </StHeader>
       )}
       {portfolioData &&
         Array.from({ length: portfolioData.totalPages }, (_, index) => (
@@ -55,12 +77,80 @@ const SearchResults = () => {
             {index + 1}
           </button>
         ))}
-    </div>
+    </>
   );
 };
 
 export default SearchResults;
 
-const Stboard = styled.div`
-  border: 1px solid black;
+const StLayout = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  justify-content: space-evenly;
+  gap: 36px;
+  width: 100%;
+  margin-top: 10px;
+`;
+
+const StItemContainer = styled.div`
+  width: 250px;
+  cursor: pointer;
+`;
+
+const StImgContainer = styled.div`
+  width: 250px;
+`;
+
+const imageStyle = `
+  width: 100%;
+  height: 310px;
+  object-fit: cover;
+  border: 1px solid;
+  border-radius: 7px;
+`;
+
+const StPortfolioImg = styled.img`
+  ${imageStyle}
+`;
+
+const StNoImg = styled.img`
+  ${imageStyle}
+`;
+
+const StDescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 3px;
+  padding: 7px 10px 10px 10px;
+  border: 1px solid;
+  border-radius: 13px;
+  background: rgba(221, 221, 221, 0.27);
+  box-shadow: inset 0px 3px 7px rgba(117, 117, 117, 0.25);
+`;
+
+const StTitleText = styled.div`
+  font-weight: bold;
+  padding-top: 8px;
+  color: ${({ theme }) => theme.color.fontColor};
+`;
+
+const StUserDescriptionContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StUserNameText = styled.div`
+  padding-left: 7px;
+  font-size: 14px;
+  color: ${({ theme }) => theme.color.fontColor};
+`;
+
+const StUserDefaultImage = styled(DefaultUserImage)`
+  width: 22px;
+  height: 22px;
+`;
+
+const StHeader = styled.div`
+  padding: 10px 45px;
 `;
