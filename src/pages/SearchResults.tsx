73e82f3225a90bf9aaf8891aade1b7be0ee73836
Undong00 <ehddjs85246@gmail.com>
2023-06-08 02:40:38 +0900

    style : 검색 결과 페이지 페이지네이션 버튼 UI 적용

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
-// import { portfolioDataState } from '@src/states/SearchResultsState';
 import { styled } from 'styled-components';
 import { searchPage } from '@src/apis/search';
 import { searchTermState } from '@src/states/SearchResultsState';
 import { useNavigate } from 'react-router-dom';
-import { PortfolioDataType } from '@src/types/portfolioType';
 import { ReactComponent as DefaultUserImage } from '@src/assets/nav/nav-default-user-image-icon.svg';
-// api 테스트 및 기능 구현 완료
-
 interface PortfolioDataContent {
   id: number;
   userProfileImage?: string;
@@ -24,15 +20,13 @@ const SearchResults = () => {
   const searchTermData = useRecoilValue(searchTermState);
   const navigate = useNavigate();
   const noImageUrl = 'public/images/no-img.jpg';
-
+  const [selectedPage, setSelectedPage] = useState(1);
   const handlePageButtonClick = async (index: number) => {
     const pageData = await searchPage(index, searchTermData);
     setPortfolioData(pageData);
+    setSelectedPage(index);
     console.log(pageData);
   };
-  // 초기 상태일 때는 검색어 x -> handlePageButtonClick => searchTermData 빈값이어서 전체 데이타 불러와지고 ->검색어 입력 searchTermState 변화 -> [searchTermData] 의존성 배열때문에  handlePageButtonClick(1); 실행됨
-
-  // const portfolioDataData = useRecoilValue(portfolioDataState);
 
   const onClickHandler = (id: number) => {
     navigate(`/detail/${id}`);
@@ -43,7 +37,7 @@ const SearchResults = () => {
   }, [searchTermData]);
 
   return (
-    <>
+    <StContainer>
       <StHeader>
         <h1>Search Result</h1>
       </StHeader>
@@ -55,7 +49,6 @@ const SearchResults = () => {
             ) : (
               <h2>모든 포트폴리오입니다.</h2>
             )}
-            {/* userProfileImage */}
           </StHeader>
           <StLayout>
             {portfolioData.content.map((portfolio, index) => (
@@ -83,17 +76,34 @@ const SearchResults = () => {
           <h2>'{searchTermData}'에 대한 포트폴리오가 없습니다.</h2>
         </StHeader>
       )}
-      {portfolioData &&
-        Array.from({ length: portfolioData.totalPages }, (_, index) => (
-          <button key={index + 1} onClick={() => handlePageButtonClick(index + 1)}>
-            {index + 1}
-          </button>
-        ))}
-    </>
+      <StbuttonContainer>
+        {portfolioData &&
+          Array.from({ length: portfolioData.totalPages }, (_, index) => (
+            <StButtonList key={index + 1}>
+              <StButton
+                onClick={() => handlePageButtonClick(index + 1)}
+                selected={index + 1 === selectedPage}
+              >
+                {index + 1}
+              </StButton>
+            </StButtonList>
+          ))}
+      </StbuttonContainer>
+    </StContainer>
   );
 };
-
 export default SearchResults;
+
+const StContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 50px; /* Add a margin-bottom to create space for the fixed button container */
+  overflow-y: auto;
+  height: calc(
+    100vh - 80px - 50px
+  ); /* Subtract the top padding and bottom margin from the full viewport height */
+`;
 
 const StLayout = styled.div`
   display: flex;
@@ -122,25 +132,16 @@ const imageStyle = `
 `;
 
 const StPortfolioImg = styled.img`
-  width: 100%;
-  height: 310px;
-  object-fit: cover;
-  border: 1px solid;
-  border-radius: 7px;
+  ${imageStyle}
 `;
 
 const StNoImg = styled.img`
-  width: 100%;
-  height: 310px;
-  object-fit: cover;
-  border: 1px solid;
-  border-radius: 7px;
+  ${imageStyle}
 `;
 
 const StDescriptionContainer = styled.div`
   display: flex;
   flex-direction: column;
-
   margin-top: 3px;
   padding: 7px 10px 10px 10px;
   border: 1px solid;
@@ -173,4 +174,59 @@ const StUserDefaultImage = styled(DefaultUserImage)`
 
 const StHeader = styled.div`
   padding: 10px 45px;
+`;
+
+const StbuttonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0;
+  padding-bottom: 12px;
+  border-top: 1px solid rgba(48, 48, 56, 0.1);
+  border-top: 1px solid rgba(var(--color-body-rgb), 0.1);
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: calc(100% - 250px);
+  background: #fff;
+  padding: 0px 0;
+  box-shadow: 0px -2px 10px rgba(0, 0, 0, 0.1);
+  z-index: 1;
+  margin-left: 250px; /* Add margin-left of 250px */
+`;
+
+const StButtonList = styled.div`
+  display: flex;
+  align-items: center; /* Add align-items: center */
+  justify-content: center; /* Add justify-content: center */
+  padding: 16px;
+  white-space: nowrap;
+  font-size: 0;
+  text-align: center;
+`;
+
+const StButton = styled.button`
+  box-sizing: border-box;
+  position: relative;
+  display: block;
+  min-width: 30px;
+  height: 28px;
+  border: 1px solid transparent;
+  border-radius: 5px;
+  padding: 0 7px;
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 26px;
+  color: #303038;
+  color: var(--color-body);
+  text-align: center;
+
+  /* Add styles for selected button */
+  ${({ selected }) =>
+    selected &&
+    `
+    border-color: green;
+    color: green;
+  `}
 `;
