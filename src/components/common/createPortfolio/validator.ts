7428318850f82c790fb6447fa36a7569b9af883a
xxyeon129 <xxyeon129@gmail.com>
2023-06-07 09:13:29 +0900

    feat: 포트폴리오 작성 step 7 소개 유효성 검사 추가

--- src/components/common/createPortfolio/validator.ts
+++ src/components/common/createPortfolio/validator.ts
@@ -2,6 +2,7 @@ const ERROR_MESSAGE = {
   TITLE_UNDER_5: '제목을 5글자 이상 입력해주세요.',
   TITLE_OVER_20: '제목을 20글자 미만으로 입력해주세요.',
   EMAIL: '올바른 이메일 주소를 입력해주세요.',
+  EXPERIENCE: '10자 이상 입력해주세요.',
 };
 
 export const validateTitle = (value: string) => {
@@ -23,4 +24,9 @@ export const validateEmail = (value: string) => {
     /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/;
   const isInvalid = !EMAIL_REGEX.test(value);
   return [isInvalid, isInvalid && ERROR_MESSAGE.EMAIL];
+};
+
+export const validateExperience = (value: string) => {
+  const isInvalid = value.length < 10;
+  return [isInvalid, isInvalid && ERROR_MESSAGE.EXPERIENCE];
 };
--- src/components/createPortfolio/createStep/Step07Introduce.tsx
+++ src/components/createPortfolio/createStep/Step07Introduce.tsx
@@ -4,17 +4,28 @@ import { styled } from 'styled-components';
 import { createExperienceState } from '@src/states';
 import { CreatePortfolioStepProps } from '@src/types/portfolioType';
 import { STEP } from '@src/constants/createPortfolioConstants';
-import useOnChangeInput from '@src/Hook/useOnChangeInput';
+import { validateExperience } from '@src/components/common/createPortfolio/validator';
 
+import useOnChangeInput from '@src/Hook/useOnChangeInput';
 import * as S from '@src/components/common/createPortfolio/createStepStyles';
 import TitleTextLabel from '@src/components/common/createPortfolio/TitleTextLabel';
 import NextStepButton from '@src/components/common/createPortfolio/NextStepButton';
 import PrevStepButton from '@src/components/common/createPortfolio/PrevStepButton';
+import ErrorMessage from '../ErrorMessage';
 
 const Step07Introduce = ({ onNextButtonClick, onPrevButtonClick }: CreatePortfolioStepProps) => {
   const [experience, setExperience] = useRecoilState(createExperienceState);
 
-  const { onChangeInput } = useOnChangeInput(setExperience);
+  const { onChangeInput, isInvalid, errorMessage } = useOnChangeInput({
+    setRecoilState: setExperience,
+    inputValue: experience,
+    validator: validateExperience,
+  });
+
+  const onClickNextButton = () => {
+    if (isInvalid) return;
+    onNextButtonClick(STEP.EIGHT);
+  };
 
   const title = '간단한 소개글을 작성해주세요';
   const description = '본인의 업무 경험을 기반으로 핵심 역량과 업무 스킬을 간단히 작성해주세요.';
@@ -26,12 +37,13 @@ const Step07Introduce = ({ onNextButtonClick, onPrevButtonClick }: CreatePortfol
         <StTextarea
           value={experience}
           onChange={onChangeInput}
-          placeholder="포트폴리오에 표시될 소개 내용을 요약하여 작성해주세요."
+          placeholder="포트폴리오에 표시될 소개 내용을 요약하여 10자 이상 작성해주세요."
         />
+        {isInvalid && <ErrorMessage errorMessage={errorMessage} />}
       </StTextareaContainer>
       <S.ButtonContainer>
         <PrevStepButton onClick={() => onPrevButtonClick(STEP.SIX)} />
-        <NextStepButton onClick={() => onNextButtonClick(STEP.EIGHT)} />
+        <NextStepButton onClick={onClickNextButton} notAllowed={`${isInvalid}`} />
       </S.ButtonContainer>
     </S.Container>
   );
@@ -45,7 +57,12 @@ const StTextarea = styled.textarea`
   width: 100%;
   min-height: 300px;
   border-radius: 10px;
-  padding: 10px;
+  padding: 15px;
+  outline: none;
+
+  &:focus {
+    outline: 2px solid;
+  }
 `;
 
 export default Step07Introduce;
