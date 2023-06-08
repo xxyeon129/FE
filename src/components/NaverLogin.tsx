17694541ad13a1e99598cf239ffee9d0b4b8fdf7
Undong00 <ehddjs85246@gmail.com>
2023-06-08 16:20:41 +0900

    style : 클라이언트 아이디 및 콜백 주소 변경

--- src/components/NaverLogin.tsx
+++ src/components/NaverLogin.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 // 함수 컴포넌트 정의
 const NaverLogin: React.FC = () => {
-  const Naver_Client_ID = '1fetknPu1x5_BOrcjE1H';
-  const Naver_Callback_Url = 'http://localhost:3000/api/users/login/naver';
-  const Naver_Auth_Url = `https://nid.naver.com/oauth2.0/authorize?client_id=${Naver_Client_ID}&response_type=code&redirect_uri=${Naver_Callback_Url}`;
+  const Naver_Client_ID = 'bA_xFHysO1Zxe8CywEoE';
+  const Naver_Callback_Url = 'http://3.34.102.60:8080/api/users/naver';
+  const Naver_Auth_Url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=bA_xFHysO1Zxe8CywEoE&redirect_uri=http://3.34.102.60:8080/api/users/naver&state=state`;
 
   // Naver_Auth_Url 연결
   const Naver = () => {
--- src/components/RedirectionNaver.tsx
+++ src/components/RedirectionNaver.tsx
@@ -5,9 +5,11 @@ import { useEffect } from 'react';
 function RedirectionNaver() {
   const code = new URL(window.location.href).searchParams.get('code');
 
-  const authNaverLogin = async code => {
+  const authNaverLogin = async (code: string) => {
     try {
-      const response = await axios.get(`/api/users/naver?code=${code}`);
+      const response = await axios.get(
+        `http://3.34.102.60:8080//api/users/naver?code=${code}state=state`
+      );
       console.log('response', response);
       return response;
     } catch (error) {
