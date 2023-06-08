f531952b6167c3ec0046e21fefe95cafb20a29c9
xxyeon129 <xxyeon129@gmail.com>
2023-06-08 15:51:47 +0900

    style: Open Sans font-family 적용

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     height: 100vh;
+    font-family: 'Open Sans', sans-serif;
   }
 
   button{
