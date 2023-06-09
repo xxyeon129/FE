ad9ea20aa2051f3308b5b7e3fd1afda56365307c
xxyeon129 <xxyeon129@gmail.com>
2023-06-09 15:48:58 +0900

    test: 테스트 코드 반영된 내용 주석처리

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -135,8 +135,8 @@ const Main = () => {
       {isExistData ? (
         <S.PortfolioListContainer>
           {list.map((item: PortfolioDataType) => (
-            // <PortfolioItem key={item.id} item={item} />
-            <TestReDesignPortfolioItem key={item.id} item={item} />
+            <PortfolioItem key={item.id} item={item} />
+            // <TestReDesignPortfolioItem key={item.id} item={item} />
           ))}
         </S.PortfolioListContainer>
       ) : (
