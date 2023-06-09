aa85dd2be13bed6d5c5f311f1ef51dab9eef5f89
xxyeon129 <xxyeon129@gmail.com>
2023-06-09 15:27:21 +0900

    fix: 배포 오류 코드 수정

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
-import ProjectModal from '@src/components/myProject/ProjectDetail';
-import AutoSearch from '@src/components/AutoSearch';
-import Project from '@src/components/myProject/CreateProject';
-import NaverLogin from '@src/components/NaverLogin';
+// import ProjectModal from '@src/components/myProject/ProjectDetail';
+// import AutoSearch from '@src/components/AutoSearch';
+// import Project from '@src/components/myProject/CreateProject';
+// import NaverLogin from '@src/components/NaverLogin';
 const Test: React.FC = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [showModal1, setShowModal1] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
+  // const [showModal1, setShowModal1] = useState(false);
 
   return (
     <>
       test
-      <button onClick={() => setShowModal(true)}>Details</button>
+      {/* <button onClick={() => setShowModal(true)}>Details</button>
       <button onClick={() => setShowModal1(true)}>Project</button>
       <ProjectModal showModal={showModal} setShowModal={setShowModal} />
       <Project showModal1={showModal1} setShowModal1={setShowModal1} />
       <AutoSearch />
-      <NaverLogin />
+      <NaverLogin /> */}
     </>
   );
 };
