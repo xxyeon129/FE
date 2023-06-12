0486bd7fa606e7bbb123da5fb2c5b5561f463f7f
Undong00 <ehddjs85246@gmail.com>
2023-06-12 21:50:40 +0900

    style : 네이버 로그인 토큰 받기 기능 추가 및 경로 수정

--- src/components/NaverLogin.tsx
+++ src/components/NaverLogin.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 // 함수 컴포넌트 정의
 const NaverLogin: React.FC = () => {
-  const Naver_Client_ID = 'bA_xFHysO1Zxe8CywEoE';
-  const Naver_Callback_Url = 'http://3.34.102.60:8080/api/users/naver';
-  const Naver_Auth_Url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=bA_xFHysO1Zxe8CywEoE&redirect_uri=http://3.34.102.60:8080/api/users/naver&state=state`;
+  // const Naver_Client_ID = 'bA_xFHysO1Zxe8CywEoE';
+  // const Naver_Callback_Url = 'http://3.34.102.60:8080/api/users/naver';
+  const Naver_Auth_Url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=bA_xFHysO1Zxe8CywEoE&redirect_uri=http://localhost:3000/api/users/naver&state=state`;
 
   // Naver_Auth_Url 연결
   const Naver = () => {
--- src/components/RedirectionNaver.tsx
+++ src/components/RedirectionNaver.tsx
@@ -4,13 +4,16 @@ import { useEffect } from 'react';
 
 function RedirectionNaver() {
   const code = new URL(window.location.href).searchParams.get('code');
+  console.log(code);
 
   const authNaverLogin = async (code: string) => {
     try {
       const response = await axios.get(
-        `http://3.34.102.60:8080//api/users/naver?code=${code}state=state`
+        `http://3.34.102.60:8080/api/users/naver?code=${code}&state=state`
       );
-      console.log('response', response);
+      console.log(response);
+      const authorization = response.headers['authorization'];
+      localStorage.setItem('authorization', authorization);
       return response;
     } catch (error) {
       console.log(error);
