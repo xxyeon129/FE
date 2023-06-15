ae77c00c6e55413d45b93158aef974c1cb1670ce
Undong00 <ehddjs85246@gmail.com>
2023-06-15 12:06:02 +0900

    style : 컴포넌트 분리 적용

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect, ChangeEvent } from 'react';
-import axios, { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import { useParams } from 'react-router-dom';
 import { styled } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { getUser, updateUser, deleteUser, updatePassword } from '@src/apis/mypageuser';
 import { useQuery, useMutation } from 'react-query';
-import { ReactComponent as EditIcon } from 'src/assets/mypage-edit.svg';
-import user from 'src/assets/nav/nav-default-user-image-icon.svg';
+import user from 'src/assets/default-user-profile-icon.svg';
 import { useSetRecoilState } from 'recoil';
 import { myPageEditState } from '@src/states/myPageEditState';
 import { ReactComponent as Close } from 'src/assets/mypage-close.svg';
-import { ReactComponent as Upload } from 'src/assets/mypage-upload.svg';
 import WithdrawalModal from '@src/components/myPage/WithdrawalModal';
 import UpdatePasswordModal from '@src/components/myPage/UpdatePasswordModal';
 import UpdateProfileModal from '@src/components/myPage/UpdateProfileModal';
-import { ReactComponent as Banner } from 'src/assets/mypage-banner.svg';
-
-interface UserData {
+import MyPageInfo from '@src/components/myPage/MypageInfo';
+import ProfileEditForm from '@src/components/myPage/ProfileEditForm';
+import PassWordEditForm from '@src/components/myPage/PassWordEditForm';
+import { useInput } from 'src/Hook/useInput';
+export interface UserData {
   nickname: string;
   email: string;
   profileImage: string;
@@ -29,21 +29,16 @@ const MyPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [isEditing, setIsEditing] = useState<Boolean>(false);
-  const [nickname, setNickname] = useState('');
+  const nickname = useInput('');
+  const oldpassword = useInput('');
+  const newpassword = useInput('');
+  const checknewpassword = useInput('');
   const [email, setEmail] = useState('');
   const [profileImage, setProfileImage] = useState<File | null | string>(null);
   const [previewImage, setPreviewImage] = useState<File | string>('');
   const [showModal, setShowModal] = useState(false);
   const [showUpdateModal, setShowUpdateModal] = useState(false);
   const [showPasswordModal, setShowPasswordModal] = useState(false);
-  const [nicknameError, setNicknameError] = useState('');
-  const [oldpassword, setOldPassword] = useState('');
-  const [newpassword, setNewPassword] = useState('');
-  const [checknewpassword, setCheckNewPassword] = useState('');
-  const [oldpasswordError, setOldPasswordError] = useState('');
-  const [newpasswordError, setNewPasswordError] = useState('');
-  const [checknewpasswordError, setCheckNewPasswordError] = useState('');
-
   const [apiError, setApiError] = useState('');
   const setMyPageEdit = useSetRecoilState(myPageEditState);
   const { data, isLoading, isError, refetch } = useQuery<UserData>('userData', () =>
@@ -52,7 +47,7 @@ const MyPage = () => {
 
   useEffect(() => {
     if (data) {
-      setNickname(data.nickname);
+      nickname.onChange({ target: { value: data.nickname } } as ChangeEvent<HTMLInputElement>);
       setEmail(data.email);
       setPreviewImage(data.profileImage || user);
     }
@@ -64,7 +59,7 @@ const MyPage = () => {
       refetch();
     },
     onError: (error: AxiosError<ErrorResponse>) => {
-      setNicknameError(error.response?.data.errorMessage as string);
+      nickname.setErrorText(error.response?.data.errorMessage as string);
     },
   });
   const deleteUserMutation = useMutation(deleteUser, {
@@ -88,25 +83,25 @@ const MyPage = () => {
   };
 
   const handleSavePassword = async () => {
-    if (oldpassword.trim() === '') {
-      setOldPasswordError('현재 비밀번호를 입력해주세요.');
+    if (oldpassword.value.trim() === '') {
+      oldpassword.setErrorText('현재 비밀번호를 입력해주세요.');
       return;
     }
 
-    if (newpassword.trim() === '') {
-      setNewPasswordError('새로운 비밀번호를 입력해주세요.');
+    if (newpassword.value.trim() === '') {
+      newpassword.setErrorText('새로운 비밀번호를 입력해주세요.');
       return;
     }
 
-    if (checknewpassword.trim() === '') {
-      setCheckNewPasswordError('비밀번호 확인을 입력해주세요.');
+    if (checknewpassword.value.trim() === '') {
+      checknewpassword.setErrorText('비밀번호 확인을 입력해주세요.');
       return;
     }
 
     const passwordData = {
-      oldPassword: oldpassword,
-      newPassword: newpassword,
-      checkNewPassword: checknewpassword,
+      oldPassword: oldpassword.value,
+      newPassword: newpassword.value,
+      checkNewPassword: checknewpassword.value,
     };
 
     await updatePasswordMutation.mutateAsync([passwordData, Number(id)]);
@@ -116,14 +111,14 @@ const MyPage = () => {
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (nickname.trim() === '') {
-      setNicknameError('닉네임을 입력하세요');
+    if (nickname.value.trim() === '') {
+      nickname.setErrorText('닉네임을 입력하세요');
       return;
     }
 
     const formData = new FormData();
     const text = JSON.stringify({
-      nickname,
+      nickname: nickname.value,
     });
     const nicknameBlob = new Blob([text], { type: 'application/json' });
     formData.append('nickname', nicknameBlob);
@@ -144,30 +139,6 @@ const MyPage = () => {
     setIsEditing(true);
     setMyPageEdit(false);
     console.log('회원정보 수정 버튼 클릭시 : ', profileImage);
-  };
-
-  const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setNickname(e.target.value);
-    setNicknameError('');
-  };
-
-  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const handleCurrentPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setOldPassword(e.target.value);
-    setOldPasswordError('');
-  };
-
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setNewPassword(e.target.value);
-    setNewPasswordError('');
-  };
-
-  const handlePasswordCheckChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setCheckNewPassword(e.target.value);
-    setCheckNewPasswordError('');
   };
 
   const handleCloseClick = () => {
@@ -193,117 +164,50 @@ const MyPage = () => {
 
   return (
     <Stdiv>
-      <StHeader isEditing={isEditing}></StHeader>
+      <StHeader isediting={isEditing}></StHeader>
       {isEditing ? (
         <StMyPageEditBox>
           <StClose>
             <Close onClick={handleCloseClick}>닫기</Close>
           </StClose>
           <StLayout>
-            <StContent>
-              <h4>프로필 수정</h4>
-              <StImageEditBox>
-                {typeof previewImage === 'string' && <StImage src={previewImage} />}
-              </StImageEditBox>
-              <label htmlFor="file">
-                <StCameraIcon />
-              </label>
-              <StFileUpload
-                type="file"
-                name="profileImage"
-                id="file"
-                onChange={handleProfileImageChange}
-                placeholder="프로필 이미지"
-              />
-              <StTextWrapper>
-                <StTitle>닉네임</StTitle>
-                <StInput
-                  type="text"
-                  name="nickname"
-                  value={nickname}
-                  onChange={handleNicknameChange}
-                  placeholder="닉네임을 적어주세요"
-                />
-                {nicknameError && <StError>{nicknameError}</StError>}
-              </StTextWrapper>
-              <StTextWrapper>
-                <StTitle>이메일</StTitle>
-                <StInput
-                  type="email"
-                  name="email"
-                  value={email}
-                  onChange={handleEmailChange}
-                  disabled
-                />
-              </StTextWrapper>
-              <div>
-                <StGoodButton onClick={handleSubmit}>변경하기</StGoodButton>
-              </div>
-            </StContent>
+            <ProfileEditForm
+              previewImage={previewImage}
+              nickname={nickname.value}
+              email={email}
+              nicknameError={nickname.error}
+              handleProfileImageChange={handleProfileImageChange}
+              handleNicknameChange={nickname.onChange}
+              handleSubmit={handleSubmit}
+            />
             <StMid></StMid>
-            <StPwContent>
-              <h4>비밀번호</h4>
-              <StTextWrapper>
-                <StTitle>현재 비밀번호</StTitle>
-                <StInput
-                  type="password"
-                  name="password"
-                  value={oldpassword}
-                  onChange={handleCurrentPasswordChange}
-                  placeholder="현재 비밀번호"
-                />
-                {oldpasswordError && <StError>{oldpasswordError}</StError>}
-              </StTextWrapper>
-              <StTextWrapper>
-                <StTitle>비밀번호</StTitle>
-                <StInput
-                  type="password"
-                  name="password"
-                  value={newpassword}
-                  onChange={handlePasswordChange}
-                  placeholder="비밀번호"
-                />
-                {newpasswordError && <StError>{newpasswordError}</StError>}
-              </StTextWrapper>
-              <StTextWrapper>
-                <StTitle>비밀번호 확인</StTitle>
-                <StInput
-                  type="password"
-                  name="passwordCheck"
-                  value={checknewpassword}
-                  onChange={handlePasswordCheckChange}
-                  placeholder="비밀번호 확인"
-                />
-                {checknewpasswordError && <StError>{checknewpasswordError}</StError>}
-                {apiError && <StError>{apiError}</StError>}
-              </StTextWrapper>
-              <div>
-                <StGoodButton onClick={handleSavePassword}>변경하기</StGoodButton>
-              </div>
-            </StPwContent>
+            <PassWordEditForm
+              handleCurrentPasswordChange={oldpassword.onChange}
+              handlePasswordChange={newpassword.onChange}
+              handlePasswordCheckChange={checknewpassword.onChange}
+              handleSavePassword={handleSavePassword}
+              oldpassword={oldpassword.value}
+              newpassword={newpassword.value}
+              checknewpassword={checknewpassword.value}
+              oldpasswordError={oldpassword.error}
+              newpasswordError={newpassword.error}
+              checknewpasswordError={checknewpassword.error}
+              apiError={apiError}
+            />
           </StLayout>
         </StMyPageEditBox>
       ) : (
-        <StMyPageBox>
-          <StImageBox>
-            {data?.profileImage ? (
-              <StImage src={data.profileImage} alt="Profile" />
-            ) : (
-              <StImage src={user} alt="Profile" />
-            )}
-          </StImageBox>
-          <StBottom>
-            <h1>{data?.nickname}</h1>
-            <p>{data?.email}</p>
-            <StEditIcon onClick={handleEditClick} />
-            <button onClick={handleWithdrawalClick}>회원탈퇴</button>
-          </StBottom>
-        </StMyPageBox>
+        <MyPageInfo
+          data={data}
+          userImage={user}
+          handleEditClick={handleEditClick}
+          handleWithdrawalClick={handleWithdrawalClick}
+        />
       )}
       {showModal && <WithdrawalModal onWithdrawal={handleWithdrawal} onClose={handleCloseModal} />}
       {showUpdateModal && <UpdateProfileModal onClose={() => setShowUpdateModal(false)} />}
       {showPasswordModal && <UpdatePasswordModal onClose={() => setShowPasswordModal(false)} />}
-      <Stbottom isEditing={isEditing} />
+      <Stbottom isediting={isEditing} />
     </Stdiv>
   );
 };
@@ -314,85 +218,19 @@ const Stdiv = styled.div`
   position: relative;
 `;
 
-const Stbottom = styled.div<{ isEditing: Boolean }>`
+const Stbottom = styled.div<{ isediting: Boolean }>`
   background: #6bf65f;
   height: 60%;
-  background-color: ${props => (props.isEditing ? '#D3D3D3' : '#6BF65F')};
+  background-color: ${props => (props.isediting ? '#D3D3D3' : '#6BF65F')};
 `;
 
-const StMyPageBox = styled.div`
-  position: absolute;
-  width: 916px;
-  height: 470px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background: rgba(255, 255, 255, 0.9);
-  border: 1px solid rgba(0, 0, 0, 0.02);
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
-  backdrop-filter: blur(10px);
-  border-radius: 15px;
-
-  @media (max-width: 768px) {
-    max-width: 90%;
-  }
-`;
-
-const StHeader = styled.div<{ isEditing: Boolean }>`
+const StHeader = styled.div<{ isediting: Boolean }>`
   width: 100%;
   height: 40%;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: ${props => (props.isEditing ? '#D3D3D3' : 'white')};
-`;
-
-// const StBanner = styled(Banner)`
-//   width: 100%;
-//   height: 100%;
-// `;
-
-const StImageBox = styled.div`
-  border: 5px solid #ffffff;
-  width: 138.42px;
-  height: 138.42px;
-  left: 815.29px;
-  top: 381.29px;
-  box-shadow: 0px 3px 9px rgba(0, 0, 0, 0.15);
-  border-radius: 100%;
-  display: flex;
-  background: #ffffff;
-  box-shadow: 0px 3px 9px rgba(0, 0, 0, 0.15);
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const StImage = styled.img`
-  width: 100%;
-  height: 100%;
-  background-size: 100%;
-  border-radius: 100%;
-`;
-
-const StBottom = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  button {
-    margin-top: 30px;
-  }
-  h1 {
-    margin-bottom: 10px;
-  }
-  p {
-    margin-bottom: 20px;
-  }
+  background-color: ${props => (props.isediting ? '#D3D3D3' : 'white')};
 `;
 
 //-------------------------------------------------------------
@@ -410,7 +248,8 @@ const StMyPageEditBox = styled.div`
 
   @media (max-width: 768px) {
     width: 90%;
-    height: 100%;
+    height: auto;
+    /* padding: 20px; */
   }
 `;
 
@@ -420,116 +259,11 @@ const StLayout = styled.div`
   justify-content: center;
   padding: 0px 68px;
   gap: 60px;
-`;
-
-const StTitle = styled.div`
-  font-weight: bold;
-  margin-bottom: 5px;
-`;
-
-const StTextWrapper = styled.div`
-  margin-bottom: 25px;
-`;
-const StInput = styled.input`
-  padding: 10px;
-  width: 308px;
-  left: 7.76%;
-  right: 55.12%;
-  top: 49.26%;
-  bottom: 43.54%;
-  background: #ffffff;
-  border: 1px solid rgba(203, 203, 203, 0.7);
-  border-radius: 6px;
 
   @media (max-width: 768px) {
-    width: 100%;
-  }
-`;
-
-const StError = styled.div`
-  font-size: 14px;
-  color: #767676;
-  padding: 0px 10px;
-`;
-
-const StFileUpload = styled.input`
-  width: 0;
-  height: 5px;
-  opacity: 0;
-`;
-
-const StImageEditBox = styled.div`
-  border: 3px solid #ffffff;
-  left: 20.75%;
-  right: 68.22%;
-  top: 22.96%;
-  bottom: 58.94%;
-  box-shadow: 0px 3px 9px rgba(0, 0, 0, 0.15);
-  width: 107px;
-  height: 107.15px;
-  border-radius: 50%;
-`;
-
-const StGoodButton = styled.button`
-  width: 126px;
-  height: 36px;
-  left: 592px;
-  top: 428px;
-  font-family: 'SUIT';
-  font-style: normal;
-  font-weight: 800;
-  font-size: 15px;
-  line-height: 35px;
-  border-radius: 8px;
-
-  text-align: center;
-
-  background-color: #6bf65f;
-  &:hover {
-    background-color: #4ae040;
-    color: white;
-  }
-`;
-
-const StEditIcon = styled(EditIcon)`
-  width: 24px;
-  height: 24px;
-  border-radius: 100%;
-
-  &:hover {
-    background-color: #d3d3d3;
-  }
-`;
-
-const StContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  h4 {
-    font-family: 'SUIT';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 20px;
-    line-height: 25px;
-    text-align: center;
-    margin-bottom: 15px;
-  }
-`;
-
-const StPwContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  h4 {
-    font-family: 'SUIT';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 20px;
-    line-height: 25px;
-    text-align: center;
-    margin-bottom: 77px;
+    flex-direction: column;
+    padding: 0;
+    gap: 20px;
   }
 `;
 
@@ -538,15 +272,10 @@ const StClose = styled.div`
   flex-direction: column;
   align-items: flex-end;
   padding: 25px;
-`;
 
-const StCameraIcon = styled(Upload)`
-  top: 50%;
-  left: 50%;
-  transform: translate(100%, -90%);
-  width: 30px;
-  height: 30px;
-  cursor: pointer;
+  @media (max-width: 768px) {
+    padding: 10px;
+  }
 `;
 
 const StMid = styled.div`
