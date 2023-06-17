1ef6295195da5db97cd7c92cf9733a352a03a9a2
xxyeon129 <xxyeon129@gmail.com>
2023-06-18 01:15:37 +0900

    feat: Kakao login 이후 로직 추가

--- src/components/socialLogin/RedirectionKakao.tsx
+++ src/components/socialLogin/RedirectionKakao.tsx
@@ -1,15 +1,24 @@
 import { useEffect } from 'react';
 import { kakaoLogin } from '@src/apis/user';
+import { useSetRecoilState } from 'recoil';
+import { loginState } from '@src/states';
+import { useNavigate } from 'react-router-dom';
+import { PATH_URL } from '@src/constants/constants';
 
 function RedirectionKakao() {
   const params = new URL(document.location.toString()).searchParams;
   const CODE = params.get('code');
+
+  const setIsLogin = useSetRecoilState(loginState);
+  const navigate = useNavigate();
 
   const authKakaoLogin = async (code: string) => {
     try {
       const response = await kakaoLogin(code);
       const accesstoken = response.headers['accesstoken'];
       localStorage.setItem('accesstoken', accesstoken);
+      setIsLogin(true);
+      navigate(PATH_URL.HOME);
       return response;
     } catch (error) {
       console.log(error);
