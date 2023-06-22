73d1136ad8427ac2eeeb1ce4e329174a7ae4a594
xxyeon129 <xxyeon129@gmail.com>
2023-06-22 12:32:20 +0900

    feat: 포트폴리오 작성 email 입력 시 도메인 select, 직접 입력 구현

--- src/components/createPortfolio/createStep/Step04PersonalInfo.tsx
+++ src/components/createPortfolio/createStep/Step04PersonalInfo.tsx
@@ -15,15 +15,17 @@ import AdditionalPersonalInfo from '../AdditionalPersonalInfo';
 import ErrorMessage from '../../common/createPortfolio/ErrorMessage';
 import useSnackbarPopup from '@src/Hook/useSnackbarPopup';
 import SnackbarPopup from '@src/components/common/SnackbarPopup';
+import EmailForm from '../EmailForm';
+import { useState } from 'react';
 
 const Step04PersonalInfo = ({ onNextButtonClick, onPrevButtonClick }: CreatePortfolioStepProps) => {
-  const [email, setEmail] = useRecoilState(createEmailState);
+  const [email, setEmail] = useRecoilState<string>(createEmailState);
 
-  const {
-    onChangeInput: onChangeEmail,
-    isInvalid: isInvalidEmail,
-    errorMessage,
-  } = useOnChangeInput({ inputValue: email, setRecoilState: setEmail, validator: validateEmail });
+  const { isInvalid: isInvalidEmail, errorMessage } = useOnChangeInput({
+    inputValue: email,
+    setRecoilState: setEmail,
+    validator: validateEmail,
+  });
 
   const { isSnackbarVisible, showSnackbarPopup } = useSnackbarPopup();
 
@@ -42,21 +44,10 @@ const Step04PersonalInfo = ({ onNextButtonClick, onPrevButtonClick }: CreatePort
     <S.Container>
       <S.ContentContainer>
         <TitleTextLabel title={title} description={description} />
-        <StInputContainer>
-          <StEmailContainer>
-            <StOutLineDiv>
-              <StInputDescription>email</StInputDescription>
-              <StPersonalInfoInput
-                type="email"
-                value={email}
-                onChange={onChangeEmail}
-                placeholder="포트폴리오에 표시될 email을 입력해주세요."
-              />
-            </StOutLineDiv>
-            {isInvalidEmail && <ErrorMessage errorMessage={errorMessage} />}
-          </StEmailContainer>
+        <StPersonalInfoForm>
+          <EmailForm isInvalidEmail={isInvalidEmail} errorMessage={errorMessage} />
           <AdditionalPersonalInfo sharedStyle={sharedStyle} />
-        </StInputContainer>
+        </StPersonalInfoForm>
       </S.ContentContainer>
 
       <S.ButtonContainer>
@@ -73,7 +64,7 @@ const Step04PersonalInfo = ({ onNextButtonClick, onPrevButtonClick }: CreatePort
   );
 };
 
-const StInputContainer = styled.div`
+const StPersonalInfoForm = styled.div`
   display: flex;
   flex-direction: column;
   gap: 2rem;
@@ -84,10 +75,6 @@ const StInputContainer = styled.div`
   }
 `;
 
-const StEmailContainer = styled.div`
-  width: 100%;
-`;
-
 const sharedStyle = `
   width: 100%;
   height: 65px;
@@ -96,27 +83,6 @@ const sharedStyle = `
   gap: 5px;
 
   padding: 10px;
-`;
-
-const StOutLineDiv = styled.div`
-  ${sharedStyle}
-
-  border: 1px solid gray;
-  border-radius: 10px;
-`;
-
-const StInputDescription = styled.div`
-  color: gray;
-  font-size: 15px;
-`;
-
-const StPersonalInfoInput = styled.input`
-  height: 100%;
-  border: none;
-
-  &::placeholder {
-    color: #b5b5b5;
-  }
 `;
 
 export default Step04PersonalInfo;
